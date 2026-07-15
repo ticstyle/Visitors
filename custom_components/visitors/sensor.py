091afb511fb5e05a78cef218a16ee2d3d0e6aec5
dualityps <4126225+ticstyle@users.noreply.github.com>
2026-07-16 00:12:59 +0200

    Update import statement for slugify function

--- custom_components/visitors/sensor.py
+++ custom_components/visitors/sensor.py
@@ -11,7 +11,7 @@ from homeassistant.core import Event, EventStateChangedData, HomeAssistant, call
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.event import async_track_state_change_event
-from homeassistant.util.slugify import slugify
+from homeassistant.util import slugify
 
 from .const import CONF_CREATE_MANUAL, CONF_TRACKERS, CONF_ZONE, DOMAIN
 
@@ -116,3 +116,4 @@ class VisitorsSensor(SensorEntity):
             if state and state.state == self._zone_state_name:
                 count += 1
         self._state = count
+        
